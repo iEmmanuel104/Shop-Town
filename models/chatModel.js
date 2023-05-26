a8068df6c935204973dbe455556bb69b941733ec
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2023-05-26 09:09:37 +0100

    new changes for chat feature

---
+++ models/chatModel.js
@@ -0,0 +1,65 @@
+module.exports = (sequelize, DataTypes) => {
+    const { User, Brand } = require('./userModel')(sequelize, DataTypes);
+    const ChatRoom = sequelize.define('ChatRoom', {
+
+        userId: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        },
+        storeId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: Brand,
+                key: 'id'
+            }
+        },
+    }, {
+        tableName: 'ChatRoom',
+        timestamps: true
+    });
+
+    const Message = sequelize.define('Message', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        senderId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        },
+        message: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    }, {
+        tableName: 'Message',
+        timestamps: true
+    });
+
+    // ASSOCIATIONS 
+    ChatRoom.associate = models => {
+        ChatRoom.hasMany(models.Message, {
+            foreignKey: 'chatRoomId',
+            as: 'messages'
+        });
+    };
+
+    Message.associate = models => {
+        Message.belongsTo(models.ChatRoom, {
+            foreignKey: 'chatRoomId',
+            as: 'chatRoom'
+        });
+    }
+
+    return { ChatRoom, Message };
+}
