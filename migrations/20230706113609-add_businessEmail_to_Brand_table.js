8c51a9a53ec65ecfc9667860b62d6a3f324a2973
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2023-07-16 00:54:02 +0100

    linting chages

--- migrations/20230706113609-add_businessEmail_to_Brand_table.js
+++ migrations/20230706113609-add_businessEmail_to_Brand_table.js
@@ -4,21 +4,19 @@ const { validate } = require('node-cron');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    //  add store settings to Store table
-    await queryInterface.addColumn('Store', 'businessEmail', {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
+    async up(queryInterface, Sequelize) {
+        //  add store settings to Store table
+        await queryInterface.addColumn('Store', 'businessEmail', {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        });
+    },
 
-
-    });
-  },
-
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Store', 'businessEmail');
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.removeColumn('Store', 'businessEmail');
+    },
 };
